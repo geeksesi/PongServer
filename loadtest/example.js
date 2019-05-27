85e8de1262a95bd9c151f59133b51c6ce2144369
mohammad javad ghasemy <geeksesi@gmail.com>
2019-05-28 01:57:23 +0430

    example of them

--- loadtest/example.js
+++ loadtest/example.js
@@ -1,32 +1,28 @@
-function requestJoinOptions(i) {
+exports.requestJoinOptions = function (i) {
     return { requestNumber: i };
 }
 
-function onJoin() {
+exports.requestJoinOptions = function()
+{
+    return {hash : "37d1084549c848bd6b76046b5fe0a7f9edef50c1b964e5fe6226d7a92e1f3f629882f8b36f520e45d3d32ea4c9d5b9a7f3d577ca18c4d957d78c6a2fda697dad"};
+}
+
+exports.onJoin = function () {
     console.log(this.sessionId, "joined.");
 }
 
-function onMessage(message) {
+exports.onMessage = function (message) {
     console.log(this.sessionId, "received:", message);
 }
 
-function onLeave() {
+exports.onLeave = function () {
     console.log(this.sessionId, "left.");
 }
 
-function onError(err) {
+exports.onError = function (err) {
     console.log(this.sessionId, "!! ERROR !!", err.message);
 }
 
-function onStateChange(state) {
+exports.onStateChange = function (state) {
     console.log(this.sessionId, "new state:", state);
 }
-
-export {
-    requestJoinOptions,
-    onJoin,
-    onMessage,
-    onLeave,
-    onError,
-    onStateChange
-};
